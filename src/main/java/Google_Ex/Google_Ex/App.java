b3a65e76333bbabda9ce3e4cdb7495191106067d
suri <you@example.com>
2019-12-31 13:05:51 +0530

    First Commit

---
+++ src/main/java/Google_Ex/Google_Ex/App.java
@@ -0,0 +1,13 @@
+package Google_Ex.Google_Ex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/qa/tests/TC_Login.java
@@ -0,0 +1,19 @@
+package com.qa.tests;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_Login {
+
+	
+	@Test
+	
+	public void lognmethod()
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+	}
+	
+}
